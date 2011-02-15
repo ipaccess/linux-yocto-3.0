405b89ed8458f95f924537d5dbe8517318fe7c9e
Misael Lopez Cruz <misael.lopez@ti.com>
2011-02-15 01:25:49 -0600

    ASoC: McPDM: Add offset cancellation
    commit 4f5e543867c6e61af6dc74c16913513376bc5f8a upstream
    
    McPDM interface supports an offset cancellation feature
    to eliminate the offset of the analog headset downlink
    path. External audio companion chip should provide
    the offset values. Offset values are fixed in this
    implementation.
    
    Change-Id: Iafbdb719b5d693bdae707717224c4be503567eeb
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>
    Integrated-by: Jingdong Lu <jingdong.lu@windriver.com>

--- sound/soc/omap/omap-mcpdm.h
+++ sound/soc/omap/omap-mcpdm.h
@@ -125,9 +125,4 @@ struct omap_mcpdm_link {
 
 struct omap_mcpdm;
 
-/*
-extern int omap_mcpdm_set_offset(struct omap_mcpdm *mcpdm,
-		int offset1, int offset2);
-*/
-
 #endif	/* End of __OMAP_MCPDM_H__ */
