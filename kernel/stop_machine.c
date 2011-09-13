0936e13cc65d816f1759e2322c5e3fc82a5037f3
Darren Hart <dvhart@linux.intel.com>
2011-09-13 12:33:32 -0700

    preempt-rt/base: correct 3.0.3->3.0.4 mismerge for, stop_machine.c
    Commit 0b805cce57f61a244eb3b8fce460b14f1be442b3 dropped a change making
    stop_cpus_mutex non-static, resulting in a build failure for 3.0.4-rt
    kernels.
    
    Restore the move to non-static from commit
    6857336c7fddaf460a13adc0c395698fcf9423ff.
    
    Reported-by: Kishore Bodke <kishore.k.bodke@intel.com>
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>
    CC: Bruce Ashfield <bruce.ashfield@windriver.com>

--- kernel/stop_machine.c
+++ kernel/stop_machine.c
@@ -150,8 +150,8 @@ void stop_one_cpu_nowait(unsigned int cpu, cpu_stop_fn_t fn, void *arg,
 	cpu_stop_queue_work(&per_cpu(cpu_stopper, cpu), work_buf);
 }
 
+DEFINE_MUTEX(stop_cpus_mutex);
 /* static data for stop_cpus */
-static DEFINE_MUTEX(stop_cpus_mutex);
 static DEFINE_MUTEX(stopper_lock);
 static DEFINE_PER_CPU(struct cpu_stop_work, stop_cpus_work);
 
