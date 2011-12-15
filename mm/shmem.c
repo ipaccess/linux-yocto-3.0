84e69f6051b31d9ac20de7a8f52edde52082ca04
Zumeng Chen <zumeng.chen@windriver.com>
2011-12-15 13:47:14 +0800

    Integrated some fixes from 3.0
    which from the following git repo.
      git://nbd.name/openwrt.git
    
    900-slab_maxsize.patch
    910-kobject_uevent.patch
    911-kobject_add_broadcast_uevent.patch
    920-unable_to_open_console.patch
    921-use_preinit_as_init.patch
    930-crashlog.patch
    940-ocf_kbuild_integration.patch
    941-ocf_20110720.patch
    950-vm_exports.patch
    960-decompress_unlzo_fix.patch
    980-update_arm_machtypes.patch
    992-mpcore_wdt_fix_watchdog_counter_loading.patch
    993-mpcore_wdt_fix_wdioc_setoptions_handling.patch
    994-mpcore_wdt_fix_timer_mode_setup.patch
    
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2951,6 +2951,16 @@ out:
 
 /* common code */
 
+void shmem_set_file(struct vm_area_struct *vma, struct file *file)
+{
+	if (vma->vm_file)
+		fput(vma->vm_file);
+	vma->vm_file = file;
+	vma->vm_ops = &shmem_vm_ops;
+	vma->vm_flags |= VM_CAN_NONLINEAR;
+}
+EXPORT_SYMBOL_GPL(shmem_set_file);
+
 /**
  * shmem_file_setup - get an unlinked file living in tmpfs
  * @name: name for dentry (to be seen in /proc/<pid>/maps
@@ -3028,11 +3038,8 @@ int shmem_zero_setup(struct vm_area_struct *vma)
 	if (IS_ERR(file))
 		return PTR_ERR(file);
 
-	if (vma->vm_file)
-		fput(vma->vm_file);
-	vma->vm_file = file;
-	vma->vm_ops = &shmem_vm_ops;
-	vma->vm_flags |= VM_CAN_NONLINEAR;
+	shmem_set_file(vma, file);
+
 	return 0;
 }
 
